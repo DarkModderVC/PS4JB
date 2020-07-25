74c9805cd3941b6ac01d7e34bc0002dd4953b944
DarkModderVC <68474547+DarkModderVC@users.noreply.github.com>
2020-07-25 16:01:26 +0530

    Create jb.js

---
+++ jb/jb.js
@@ -0,0 +1,7 @@
+pivot(ropchain);
+var main_ret = read_ptr_at(main_ret);
+var printf_buf_end = read_ptr_at(ropchain+printf_buf_offset);
+var printf_ans = read_mem_as_string(printf_buf, printf_buf_end-printf_buf);
+var _ = malloc_nogc.pop();
+var _ = malloc_nogc.pop();
+var _ = malloc_nogc.pop();
