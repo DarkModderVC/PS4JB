f3d5bf14d83aac037b32215603cbff6b1fabeced
DarkModderVC <68474547+DarkModderVC@users.noreply.github.com>
2021-04-10 16:35:51 +0530

    PS4 Jailbreak 7.5X GoldHEN V1.1 Stability Update

--- 75XV2/webkit/ps4.js
+++ 75XV2/webkit/ps4.js
@@ -199,7 +199,11 @@ function toggle_payload(pld){
 		preloadScripts(['payloads/preloader.js', 'payloads/ps4debug.js', 'payloads/loader.js']);
 	}else if(pld == "goldhen"){
 		document.getElementById("progress").innerHTML="Loading Payload.. Please wait..";
-		preloadScripts(['payloads/preloader.js', 'payloads/goldhen.js', 'payloads/loader.js']);
+		if(fw=="755"){
+			preloadScripts(['payloads/preloader.js', 'payloads/goldhen'+fw+'.js', 'payloads/loader.js']);
+		}else{
+			preloadScripts(['payloads/preloader'+fw+'.js', 'payloads/goldhen'+fw+'.js', 'payloads/loader.js']);	
+		}
 	}else if(pld == "webrte"){
 		document.getElementById("progress").innerHTML="Loading Payload.. Please wait..";
 		preloadScripts(['payloads/preloader.js', 'payloads/webrte.js', 'payloads/loader.js']);
